ee6acc4eb185f557bd7e500d8e1ee019583f26f8
teru <hamusutaateru@live.jp>
2019-12-08 09:28:16 +0900

    add objects with commons

--- ai/src/objects.rs
+++ ai/src/objects.rs
@@ -34,12 +34,52 @@ pub struct Robot {
     pub acceleration: Vec3,
 }
 
+impl Default for Robot {
+    fn default() -> Robot {
+        Robot {
+            position: vec3(0.0, 0.0, 0.0),
+            velocity: vec3(0.0, 0.0, 0.0),
+            acceleration: vec3(0.0, 0.0, 0.0),
+        }
+    }
+}
+
+impl Robot {
+    pub fn new(p: Vec3, v: Vec3, a: Vec3) -> Robot {
+        Robot {
+            position: p,
+            velocity: v,
+            acceleration: a,
+        }
+    }
+}
+
 #[derive(Debug, Copy, Clone)]
 pub struct Ball {
     //x,y
     pub position: Vec2,
     pub velocity: Vec2,
     pub acceleration: Vec2,
+}
+
+impl Default for Ball {
+    fn default() -> Ball {
+        Ball {
+            position: vec2(0.0, 0.0),
+            velocity: vec2(0.0, 0.0),
+            acceleration: vec2(0.0, 0.0),
+        }
+    }
+}
+
+impl Ball {
+    pub fn new(p: Vec2, v: Vec2, a: Vec2) -> Ball {
+        Ball {
+            position: p,
+            velocity: v,
+            acceleration: a,
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
